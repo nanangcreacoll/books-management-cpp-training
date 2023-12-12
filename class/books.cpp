d16ff892c1e0c4df09b4b8b07061e3f4f3f83364
nanangcreacoll <nanang.creacol.26@gmail.com>
2023-12-13 02:37:58 +0800

    add: class book

---
+++ class/books.cpp
@@ -0,0 +1,26 @@
+#include "books.hpp"
+
+void books::add()
+{
+
+}
+
+void books::update_price()
+{
+
+}
+
+void books::search()
+{
+
+}
+
+void books::update()
+{
+    
+}
+
+void books::display()
+{
+
+}
---
+++ main.cpp
@@ -0,0 +1,41 @@
+// Header Files
+#include <iostream>
+#include <string>
+#include <mysql/mysql.h>
+#include <sstream>
+#include <conio.h>
+
+// Definition
+#define HOST "127.0.0.1"
+#define MYSQL_PASS "creacoll"
+#define DATABASE "book-management"
+#define MYSQL_PORT 3306
+#define PASSWORD "creacoll"
+
+using namespace std;
+
+//Global Variable
+MYSQL *conn;
+MYSQL_RES *res_set;
+MYSQL_ROW row;
+stringstream stmt;
+const char* q;
+string query;
+
+typedef struct
+{
+    int date;
+    int month;
+    int year;
+}date;
+
+//classes
+#include "class/books.hpp"
+
+
+int main()
+{
+    cout << "hello world" << endl;
+
+    return 0;
+}
