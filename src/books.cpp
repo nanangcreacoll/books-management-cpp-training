f9ead1a07b599586db110580a3cad500f059817a
nanangcreacoll <nanang.creacol.26@gmail.com>
2023-12-14 01:43:35 +0800

    update feat: books update price

--- src/books.cpp
+++ src/books.cpp
@@ -17,6 +17,7 @@ void books::add()
     query = stmt.str();
     q = query.c_str();
     mysql_query(conn, q);
+    res_set = mysql_store_result(conn);
 
     if (!(res_set))
         cout << endl << endl << "Book Record Inserted Successfully" << endl << endl << endl;
@@ -26,7 +27,49 @@ void books::add()
 
 void books::update_price()
 {
+    char choice;
+    cout << "\nBook Update in Price" << endl;
+    cout << "\tBook Id: ";
+    cin >> id;
+    
+    stmt.str("");
+    stmt << "SELECT name, price FROM books WHERE id = " << id << ";";
+    query = stmt.str();
+    q = query.c_str();
+    mysql_query(conn, q);
 
+    res_set = mysql_store_result(conn);
+    
+    if ((row = mysql_fetch_row(res_set)) != NULL)
+    {
+        cout << "Book name: " << row[0] << endl;
+        cout << "Current price of the book: " << row[1] << endl;
+        cout << "Do yout want to update the price? [y/n]: ";
+        cin >> choice;
+        
+        if (choice == 121 || choice == 89)
+        {
+            cout << "New price: ";
+            cin >> price;
+
+            stmt.str("");
+            stmt << "UPDATE books SET price = " << price << "WHERE id = " << id << ";";
+            res_set = mysql_store_result(conn);
+
+            if (!(res_set))
+                cout << endl << endl << "Book Price Updated Successfully" << endl << endl << endl;
+            else
+                cout << endl << endl << "Entry ERROR!" << endl << "Contact Technical Team" << endl << endl << endl;
+        }
+        else
+        {
+            cout << "No changes made." << endl;
+        }
+    }
+    else
+    {
+        cout << "No book found." << endl;
+    }
 }
 
 void books::search()
