3b4ed0cb5fccc335d1c00b3201290bfb83c7b424
nanangcreacoll <nanang.creacol.26@gmail.com>
2023-12-14 23:20:56 +0800

    add: new class employees

---
+++ src/employees.hpp
@@ -0,0 +1,36 @@
+#ifndef __EMPLOYEES
+#define __EMPLOYEES
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <mysql/mysql.h>
+
+//struct
+#include "dateStruct.hpp"
+
+using namespace std;
+
+class employees
+{
+    int id;
+    string name;
+    string addr_line1;
+    string addr_line2;
+    string addr_city;
+    string addr_state;
+    long int phone_num;
+    date date_of_joining;
+    long int salary;
+    string mgr_status;
+
+public:
+    void add_emp();
+    void search_emp();
+    void assign_mgr_status();
+    void display();
+    void update_salary();
+};
+
+
+#endif
