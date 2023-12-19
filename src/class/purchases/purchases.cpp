302e9a7fc3b5b7aff62599ab5c4289b70d3f6777
nanangcreacoll <nanang.creacol.26@gmail.com>
2023-12-20 01:43:01 +0800

    feat: purchases view

--- src/class/purchases/purchases.cpp
+++ src/class/purchases/purchases.cpp
@@ -23,17 +23,17 @@ void purchases::new_ord()
 
 void purchases::mark_reciv()
 {
-    cout << "Order Recieved Mark" << endl;
+    cout << "Order Received Mark" << endl;
     cout << "Order id: ";
     cin >> order_id;
     
     stmt.str("");
-    stmt << "UPDATE purchases SET recieved = 'T' WHERE order_id = " << order_id << ";";
+    stmt << "UPDATE purchases SET received = 'T' WHERE order_id = " << order_id << ";";
     query = stmt.str();
     q = query.c_str();
     mysql_query(conn, q);
 
-    cout << "Recieved marked successfully!" << endl;
+    cout << "Received marked successfully!" << endl;
 }
 
 void purchases::mark_cancel()
@@ -43,10 +43,65 @@ void purchases::mark_cancel()
     cin >> order_id;
     
     stmt.str("");
-    stmt << "UPDATE purchases SET recieved = 'C' WHERE order_id = " << order_id << ";";
+    stmt << "UPDATE purchases SET received = 'C' WHERE order_id = " << order_id << ";";
     query = stmt.str();
     q = query.c_str();
     mysql_query(conn, q);
 
     cout << "Cancelled marked successfully" << endl;
+}
+
+void purchases::view()
+{
+    int c;
+    cout << "Select an Option" << endl;
+    cout << "1. View orders not Received" << endl;
+    cout << "2. View orders Cancelled" << endl;
+    cout << "3. View orders Received" << endl;
+    cout << "\nEnter your choice: ";
+    cin >> c;
+    switch (c)
+    {
+    case 1:
+        received = 'F';
+        break;
+    case 2:
+        received = 'C';
+        break;
+    case 3:
+        received = 'T';
+        break;
+    default:
+        return;
+    }
+
+    stmt.str("");
+    stmt << "SELECT * FROM purchases WHERE received = " << received << ";";
+    query = stmt.str();
+    q = query.c_str();
+    mysql_query(conn, q);
+    res_set = mysql_store_result(conn);
+    
+    switch (c)
+    {
+    case 1:
+        cout << endl << "Orders not received are" << endl;
+        break;
+    case 2:
+        cout << endl << "Orders cancelled are" << endl;
+    case 3:
+        cout << endl << "Orders received are" << endl;
+    }
+    while ((row = mysql_fetch_row(res_set)) != NULL)
+    {
+        cout << endl;
+        cout << "Order id: " << row[0] << endl;
+        cout << "Book id: " << row[1] << endl;
+        cout << "Supplier id: " << row[2] << endl;
+        cout << "Quantity: " << row[3] << endl;
+        cout << "Date ordered: " << row[4] << endl;
+        cout << "Estimated delivery date: " << row[5] << endl;
+        cout << endl << endl << endl;
+    }
+    cout << endl << endl << endl << endl;
 }
