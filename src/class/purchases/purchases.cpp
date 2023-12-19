8b99d1c562ace168ae00f292d3f69a77b57e78c8
nanangcreacoll <nanang.creacol.26@gmail.com>
2023-12-20 00:14:17 +0800

    feat: purchases add new order

--- src/class/purchases/purchases.cpp
+++ src/class/purchases/purchases.cpp
@@ -1,1 +1,22 @@
 #include "purchases.hpp"
+
+void purchases::new_ord()
+{
+    cout << "New Order Books" << endl;
+    cout << "Book id: ";
+    cin >> book_id;
+    cout << "Supplier id: ";
+    cin >> sup_id;
+    cout << "Quantity: ";
+    cin >> qty;
+    cout << "Estimated expected delivery (in days): ";
+    cin >> estim_deliv;
+    
+    stmt.str("");
+    stmt << "INSERT INTO purchases (book_id, sup_id, qty, dt_ordered, estim_deliv) VALUES(" << book_id << ", " << sup_id << ", " << qty << ", curdate(), Date_add(curdate(), INTERVAL " << estim_deliv << " DAY));";
+    query = stmt.str();
+    q = query.c_str();
+    mysql_query(conn, q);
+
+    cout << "New order added!" << endl;
+}
