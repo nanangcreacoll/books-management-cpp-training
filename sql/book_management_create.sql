ffbd907666dfcf6e5dbd48860b69b21110a637bf
nanangcreacoll <nanang.creacol.26@gmail.com>
2023-12-21 03:25:42 +0800

    add sql query

--- sql/book_management_create.sql
+++ sql/book_management_create.sql
@@ -1,4 +1,4 @@
--- Active: 1702918408133@@127.0.0.1@3306
+-- Active: 1702918408133@@127.0.0.1@3306@book_management
 
 DROP DATABASE book_management;
 CREATE DATABASE book_management
@@ -21,9 +21,65 @@ CREATE TABLE suppliers
 (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
-    phone_nume BIGINT,
+    phone_num BIGINT,
+    addr_line1 VARCHAR(255),
+    addr_line2 VARCHAR(255),
+    addr_city VARCHAR(255),
+    addr_state VARCHAR(255)
+);
+
+--purchases
+CREATE TABLE purchases
+(
+    order_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    book_id INT,
+    sup_id INT,
+    qty INT,
+    dt_ordered DATE,
+    estim_deliv INT,
+    received CHAR,
+    FOREIGN KEY (book_id) REFERENCES books(id),
+    FOREIGN KEY (sup_id) REFERENCES suppliers(id)
+);
+
+--employees
+CREATE TABLE employees
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
     addr_line1 VARCHAR(255),
     addr_line2 VARCHAR(255),
     addr_city VARCHAR(255),
     addr_state VARCHAR(255),
+    phone_num BIGINT,
+    date_of_joining DATE,
+    salary BIGINT,
+    mgr_status VARCHAR(255)
+);
+
+--members
+CREATE TABLE members
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    addr_line1 VARCHAR(255),
+    addr_line2 VARCHAR(255),
+    addr_city VARCHAR(255),
+    addr_state VARCHAR(255),
+    phone_num BIGINT,
+    beg_date DATE,
+    end_date DATE,
+    valid VARCHAR(255)
+);
+
+CREATE TABLE sales
+(
+    invoice_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id INT,
+    book_id INT,
+    qty INT,
+    amount INT,
+    date_sales DATE,
+    FOREIGN KEY (member_id) REFERENCES members(id),
+    FOREIGN KEY (book_id) REFERENCES books(id)
 );
---
+++ sql/select_query.sql
@@ -0,0 +1,13 @@
+-- Active: 1702918408133@@127.0.0.1@3306@book_management
+
+SELECT * FROM books;
+
+SELECT * FROM suppliers;
+
+SELECT * FROM employees;
+
+SELECT * FROM purchases;
+
+SELECT * FROM members;
+
+SELECT * FROM sales;
